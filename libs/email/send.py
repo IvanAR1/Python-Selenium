9d162d9fe1be260b733525b9287bb219bd290a12
11256880 <danilo.alanis@autozone.com>
2025-02-18 14:19:42 -0600

    Solved bug send only one mail (libs/email/send

--- libs/email/send.py
+++ libs/email/send.py
@@ -6,15 +6,17 @@ from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from ..path.path_utils import BaseName
 
-def globalEmail(mail:smtplib.SMTP, subject:str, body:str, from_email:str, to_email:str, cc_email:str=None, cco_email:str=None, files:list = None):
+def globalEmail(mail:smtplib.SMTP, subject:str, body:str, from_email:str, to_email:str, cc_email:str=None, cco_email:str=None, files:list = None, mime_type:str="plain"):
     try:
         message = MIMEMultipart()
         message['From'] = from_email
         message['To'] = (',').join(to_email.split(';'))
-        message['CC'] = (',').join(cc_email.split(';')) if cc_email is not None else None
-        message['CCO'] = (',').join(cco_email.split(';')) if cco_email is not None else None
+        if cc_email: 
+            message['Cc'] = (',').join(cc_email.split(';')) 
+        if cco_email: 
+            message['Bcc'] = (',').join(cco_email.split(';'))
         message['Subject'] = subject
-        message.attach(MIMEText(body, 'plain'))
+        message.attach(MIMEText(body, mime_type))
         if(files is not None):
             for key, file in enumerate(files):
                 file_name = None
@@ -32,16 +34,18 @@ def globalEmail(mail:smtplib.SMTP, subject:str, body:str, from_email:str, to_ema
                     encoders.encode_base64(part)
                     part.add_header('Content-Disposition', f'attachment; filename={file_name}')
                     message.attach(part)
-        mail.sendmail(from_email, to_email, message.as_string())
+        list_emails = f'{message["To"]}, {message["Cc"]}, {message["Bcc"]}'.replace(" ","").split(",")
+        list_emails = list(filter(lambda x: x not in "None", list_emails))
+        mail.sendmail(from_email, list_emails, message.as_string())
     except smtplib.SMTPException as e:
         raise Exception("Error al enviar el correo: %s" % e)
     except Exception as e:
         raise Exception("Error al enviar el correo: %s" %e)
     
-def onlyText(mail:smtplib.SMTP, subject:str, body:str, from_email:str, to_email:str, cc_email:str=None, cco_email:str=None):
-    globalEmail(mail, subject, body, from_email, to_email, cc_email, cco_email)
+def onlyText(mail:smtplib.SMTP, subject:str, body:str, from_email:str, to_email:str, cc_email:str=None, cco_email:str=None, mime_type:str="plain"):
+    globalEmail(mail, subject, body, from_email, to_email, cc_email, cco_email, mime_type=mime_type)
     
-def withFiles(mail:smtplib.SMTP, subject:str, body:str, from_email:str, to_email:str, files:list|str, cc_email:str="", cco_email:str=""):
+def withFiles(mail:smtplib.SMTP, subject:str, body:str, from_email:str, to_email:str, files:list|str, cc_email:str="", cco_email:str="", mime_type:str="plain"):
     if isinstance(files, str):
         files = [{"file_ubication":files, "file_name":BaseName(files)}]
-    globalEmail(mail, subject, body, from_email, to_email, cc_email, cco_email, files)
+    globalEmail(mail, subject, body, from_email, to_email, cc_email, cco_email, files, mime_type)
